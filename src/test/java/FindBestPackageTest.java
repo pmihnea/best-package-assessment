a6e98b6c5a92fd0b1d0d53aa087060b418bbd242
mihnea.pretorian <mihnea.pretorian@wirecard.com>
2020-09-16 18:56:36 +0200

    fixed path extraction from url on linux

--- src/test/java/FindBestPackageTest.java
+++ src/test/java/FindBestPackageTest.java
@@ -1,18 +1,29 @@
 import com.google.common.io.Resources;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.Test;
+
+import java.net.URL;
 
 // Checks that no exceptions occur when executing the main program
 public class FindBestPackageTest {
+    private String getPath(URL url){
+        String path = url.getPath();
+        if(SystemUtils.IS_OS_WINDOWS){
+            path = path.substring(1); //remove the leading '/'
+        }
+        return path;
+    }
+
     @Test
     public void testOk() {
-        String path = Resources.getResource("sampleInput.txt").getPath().substring(1); //remove the leading '/'
+        String path = getPath(Resources.getResource("sampleInput.txt"));
         String[] args = {path};
         FindBestPackage.main(args);
     }
 
     @Test
     public void testWithErrors() {
-        String path = Resources.getResource("sampleInvalidInput.txt").getPath().substring(1); //remove the leading '/'
+        String path = getPath(Resources.getResource("sampleInvalidInput.txt"));
         String[] args = {path};
         FindBestPackage.main(args);
     }
